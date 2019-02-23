28bd83f946a53f9d61cd04ca3cddfd5f28a9c2e0
David Collins <b4time@pacbell.net>
2019-02-22 17:17:23 -0800

    Initial Commit

---
+++ ibim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ibim
+//
+//  Created by David Collins on 2/22/19.
+//  Copyright © 2019 David Collins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
